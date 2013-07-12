df04e4d1366edfa14f29baa96ecdb1d966285fda
Eric Huang <eric@erichuang.info>
2013-07-12 15:36:54 +1000

    minor

--- spec/kd_tree_spec.rb
+++ spec/kd_tree_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "KdTree" do
+describe KdTree do
   it "initializes" do
     t = KdTree.new([[1, 20], [30, 4], [50, 60], [7, 80], [90, 10]])
   end
